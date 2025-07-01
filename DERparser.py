9c6e69b29da3505060b6ffe8923de3bbef760899
smh43 <noe.willemain@cdfnancy.fr>
2025-07-01 20:15:46 +0200

    Add files via upload

--- DERparser.py
+++ DERparser.py
@@ -14,7 +14,8 @@ DER fonctionne en encodant des blocks de données en 3 paramètres comme :
 
     Ce parser ordonne par indentation, pour chaque séquence une identation supplémentaire est ajouté
 
-    Types restants: BIT STRING, OBJECT IDENTIFIER, UTF8STRING, SET, PrintableString, IA5String, UTCTIME, GeneralizedTime
+    Types restants:UTF8STRING, SET, UTCTIME, GeneralizedTime
+    Objet Identifier est a corriger, il faut concatener les bits en un nombre en dehors du 7eme bit
 
     Egalement le parser n'accepte pour l'instant que le DER en bytes
 """
@@ -26,15 +27,17 @@ class DER:
             raise(TypeError, "La clé DER doit être de type byte")
         self.key = key
 
+
     def decode(self) -> str:
         index = 0
         asn = []
 
         while(index < len(self.key)): #index va varier car il va s'incrémenter à chaque block
-            out, index = self.readBlock(index)
-            asn.append(out)
+            Bloc, index = self.readBlock(index)
+            asn.append(Bloc)
             
         return "".join(asn)
+
 
     def getLength(self, taille : int, index : int) -> list[int, int]: #retourne longueur et offset
 
@@ -51,48 +54,82 @@ class DER:
             
             return int.from_bytes(bytes(tabTaille), 'big'), (taille & 0b01111111) + 1 #retourne la taille, et l'offset de tous les octets de taille (+1 est le premier)
 
+
+    def setLengthAndOffset(self, peek : int, indexType : int) -> list[int, int]: #retourne la longueur de la valeur, et l'offset à partir du type
+        taille, offset = self.getLength(peek, indexType) 
+        return taille, offset + 1 #skip les octets de tailles
+
+
     def readBlock(self, indexType: int, indent=False) -> list[str, int]: #retourne la valeur de l'élément, et le nouvel index
 
         if(indexType < len(self.key)-1):
-            peek = self.key[indexType+1]
+            peek = self.key[indexType+1] #peek va regarder l'octet suivant
         else:
-            #gestion des erreurs mauvaises parce que justement tous les types ne sont pas définis
+            #mauvaise gestion des erreurs parce que justement tous les types ne sont pas définis
             raise(OverflowError, "Un type est défini sur le dernier octet")
+            
+        _type = self.key[indexType]
+
+        taille, offset = self.setLengthAndOffset(peek, indexType)
 
         Block="\n"
-        offset = 1
+        if(indent): Block += '\t'
         
-        if(self.key[indexType] == 0x30 or self.key[indexType] == 0x10): #struct
-            Block +="SEQUENCE: "
-            indexSequence = indexType
-            taille, o = self.getLength(peek, indexType)
-            offset += o #skip les octets de tailles
-
-            indexSequence+=offset
+        if(_type == 0x30 or _type == 0x10): #Sequence (l'équivalent d'une struct)
+            Block += "SEQUENCE: "
+            indexSequence = indexType + offset
 
             while(indexSequence + offset < indexType + taille):
                 statement, indexSequence = self.readBlock(indexSequence, True)
                 Block += statement
             return Block, indexSequence + offset
         
-        elif(self.key[indexType] == 0x05 and peek == 0): #NULL
-            if(indent): Block += '\n'
-            Block += "NULL"
-            return Block, indexType + 2
+        elif(_type == 0x11 or _type == 0x31):
+            print("SET")
+            return "", indexType + 1
         
-        elif(self.key[indexType] == 0x02): #INT
+        elif(_type == 0x06): #Object Identifier
 
-            if(indent):  Block += '\t'
+            Block += "OBJECT IDENTIFIER : "
+
+            value = self.key[indexType + offset]
+            x = value // 40
+            y = value % 40
+            offset += 1
+
+            tabInt = [] 
+            id = [] 
+            id.append(str(x))
+            id.append(str(y))
+
+            for i in range(1, taille): #1er octet déjà consumé
+                octet = self.key[indexType + offset]
+                tabInt.append(octet & 0b01111111)
+
+                if(octet & 0b10000000 == 0 or i == taille - 1): #fin de l'entier
+                    nb = int.from_bytes(tabInt, 'big')
+                    id.append(str(nb))
+                    tabInt = []
+
+                offset += 1
+
+            Block += ".".join(id)
+
+            print(Block)
+
+            return Block, indexType + offset
+        
+        elif(_type == 0x05 and peek == 0): #NULL
+
+            return "NULL", indexType + 2
+        
+        elif(_type == 0x02): #INT
+
             Block += "INTEGER : "
 
-            offset = 1
             tabInt = []
 
-            taille, o = self.getLength(peek, indexType)
-            offset += o #skip les octets pour la taille
-
             for i in range(0, taille):
-
                 tabInt.append(self.key[indexType + offset])
                 offset+=1
   
@@ -102,15 +139,37 @@ class DER:
             Block += '\n'
             return Block, indexType + offset
         
-        elif(self.key[indexType] == 0x04): #string
-            if(indent): Block +='\t'
-            Block += "STRING: "
+        elif (_type == 0x03): #bit string
 
-            offset = 1 #skip le type
-            tabChar = []
+            Block += "BITSTRING ("
+
+            padding = self.key[indexType + offset] #le bit string commence par un octet qui défine un padding
+            offset += 1
+
+            Block += f"padding de {padding} bits) : "
+
+            chaine = ""
             
-            taille, o = self.getLength(peek, indexType)
-            offset += o #skip les octets pour la taille
+            for i in range(0, taille):
+                    char = hex(self.key[indexType + offset])
+                    offset += 1
+                    chaine += char + " "
+
+            Block += chaine.strip()
+
+            return Block, indexType + offset
+        
+        elif(_type == 0x04 or _type == 0x13 or _type == 0x16): #string sur 1 octet
+
+            match(_type):
+                case 0x04:
+                    Block += "OCTET STRING: "
+                case 0x13:
+                    Block += "PRINTABLE STRING: "
+                case 0x16:
+                    Block += "IA5 STRING: "
+
+            tabChar = []
 
             for i in range(0, taille):
                 tabChar.append(bytes(self.key[indexType + offset]).decode("ascii"))
@@ -120,6 +179,19 @@ class DER:
             Block+='\n'
 
             return Block, indexType + offset
+        
+        elif(_type == 0x12): #utf-8
+            print("UTF-8")
+            return "", indexType + 1
+        
+        elif(_type == 0x17): #UTCTime
+            print("UTCTime")
+            return "", indexType + 1
+        
+        elif(_type == 0x18): #Generalized time
+            print("Generalized time")
+            return "", indexType + 1
+
         else:
             return "", indexType + 1 #type indéfini 
         
